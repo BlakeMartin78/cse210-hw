95941e79781ec7a6847f84bda96a48a68b51e172
Blake Martin <thesignupemail7@gmail.com>
2023-04-24 12:18:31 -0600

    Did Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        
+        //Console.WriteLine($"The magic number is {number}");
+        
+        string readguess = "-1";
+        int guess = int.Parse(readguess);
+
+        while (guess != number)
+        {
+
+            Console.WriteLine("What is your guess?");
+            readguess = Console.ReadLine();
+            guess = int.Parse(readguess);
+
+            if (guess == number)
+            {
+                Console.WriteLine("Gj Bro");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+        }
     }
 }
