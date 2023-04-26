ba2053c1f07ac651d7f23422bd6bcb1f750ef042
Blake Martin <thesignupemail7@gmail.com>
2023-04-26 12:29:18 -0600

    Finished Prep 4, Abstract example

---
+++ abstract-example/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+Book mistborn = new Book("Mistborn: The Final Empire", "Brandon Sanderson");
+Book eragon = new Book("Eragon","Christopher Paolini");
+
+
+mistborn.Display();
+Console.WriteLine(mistborn.IsAvailable());
+eragon.Display();
+Console.WriteLine(mistborn.IsAvailable());
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, at least 1, and type 0 when finished.");
+        string input;
+        int inputNumber = -1;
+        
+        
+        while (inputNumber != 0)
+        {
+            Console.Write("Enter number : ");
+            input = Console.ReadLine();
+            inputNumber = int.Parse(input);
+
+            if (inputNumber != 0)
+            {
+            numbers.Add(inputNumber);
+            }
+        }
+        float average = 0;
+        int sum = 0;
+        int biggest = -1;
+        foreach (int number in numbers)
+        {
+            sum += number;
+            if (biggest < number)
+            {
+                biggest = number;
+            }
+        }
+        
+        
+        average = sum / numbers.Count;
+        
+
+
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The biggest number is {biggest}");
     }
 }
