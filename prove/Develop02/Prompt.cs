dd65ccf00e2a379f8f2eb223a23709006e696219
Blake Martin <thesignupemail7@gmail.com>
2023-05-03 15:33:51 -0600

    Partway done with prove

---
+++ prove/Develop02/Prompt.cs
@@ -0,0 +1,17 @@
+public class Prompt
+{
+
+    List<string> _prompts = new List<string>();
+
+    public Prompt()
+    {
+        _prompts.Add("How was your day?");
+    }
+
+
+
+    public string getRandomPrompt()
+    {
+        return "this is a promptu";   
+    }
+}
