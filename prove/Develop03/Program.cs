ac1bcfa90f3e7a4b1cf1d73ac0dad41e93815f57
Blake Martin <thesignupemail7@gmail.com>
2023-05-27 19:10:43 -0600

    Finished scripture memorizer

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference scriptReference = new Reference("Doctrine & Covenants",36,6);
+        Scripture dnc6 = new Scripture(scriptReference, "Look unto me in every thought; doubt not, fear not.");
+        string quitCheck = "";
+        Console.WriteLine("To continue, press enter. To quit, type Quit.");
+        
+        int firstTime = 0;
+        
+        while ((quitCheck.ToUpper() != "QUIT") && (dnc6.isAllHidden() != true))
+        {
+        
+            if (firstTime == 0) // This if/else exists purely to skip the first instance, it's kinda ugly though.
+            {
+                firstTime++;
+            }
+            else
+            {
+                dnc6.HideRandomWord();
+            }
+
+            dnc6.Display();
+            quitCheck = Console.ReadLine();
+            
+            Console.Clear();
+        }
     }
 }
