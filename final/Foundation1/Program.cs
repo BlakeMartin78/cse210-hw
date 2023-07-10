2ea97ee645cc558498708dcb9ce5d0c9435b3b81
Blake Martin <thesignupemail7@gmail.com>
2023-07-09 22:08:42 -0600

    Finished Foundation1

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        List<Video> videos = new List<Video>();
+        videos.Add(new Video("Sweet music video", "Death Arcana", 200));
+        videos.Add(new Video("Funny meme", "Dead Channel", 20));
+        videos.Add(new Video("New Super Mario Brothers Lets Play Part 43", "MarioLover47", 2732));
+
+
+        videos[0].AddComment("Sweet tunes", "Abby");
+        videos[0].AddComment("What a jam!", "Josephine");
+        videos[0].AddComment("Pog", "Twitch.tv");
+
+        videos[1].AddComment("Old meme :(", "Blake");
+        videos[1].AddComment("CAT!", "Abby");
+        videos[1].AddComment("Click this link for free money!", "Scammer");
+
+        videos[2].AddComment("Dude this is the fourth NG+ in this series, pick a new name", "A hater");
+        videos[2].AddComment("Please leave the negativity out of my channel.", "MarioLover47");
+        videos[2].AddComment("I didn't know you were chill like that g", "A hater");
+
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.Clear();
+        Console.WriteLine();
+        Console.WriteLine();
+        foreach (Video video in videos)
+        {
+            video.Display();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+        Console.WriteLine();
     }
 }
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -5,18 +5,30 @@ string _author;
 int _length;
 List<Comment> _comments = new List<Comment>();
 
-public Video()
+public Video(string title, string author, int length)
 {
-
+    _title = title;
+    _author = author;
+    _length = length;
 }
 
-public void AddComment(string commentor, string text)
+public void AddComment(string text, string commentor)
 {
     Comment currentComment = new Comment(commentor,text);
     _comments.Add(currentComment);
 }
 
-
+public void Display()
+{
+    Console.WriteLine($"{_title} by {_author}");
+    Console.WriteLine($"{_length} seconds");
+    Console.WriteLine("");
+    Console.WriteLine("Comments:");
+    foreach (Comment comment in _comments)
+    {
+        Console.WriteLine(comment.Display());
+    }
+}
 
 
 }
